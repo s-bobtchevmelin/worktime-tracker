e48ef7ac7a603496718aa9dac7d23d9c546861c3
Sarah B.M <sarah.melin19403@gmail.com>
2024-07-08 09:09:01 +0200

    worked period controller

---
+++ app/Http/Controllers/WorkedPeriodController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkedPeriod;
+use Illuminate\Http\Request;
+
+class WorkedPeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WorkedPeriod $workedPeriod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, WorkedPeriod $workedPeriod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WorkedPeriod $workedPeriod)
+    {
+        //
+    }
+}
--- database/migrations/2024_07_06_091524_create_worked_periods_table.php
+++ database/migrations/2024_07_06_091524_create_worked_periods_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('period_worked', function (Blueprint $table) {
+        Schema::create('worked_periods', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->date('date');
