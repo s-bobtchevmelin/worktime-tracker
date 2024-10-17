271e077949c8172332b8d33e654194ad9d03b7a7
Sarah B.M <sarah.melin19403@gmail.com>
2024-10-17 13:35:49 +0200

    fix delete @transfer

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,28 +6,21 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import {createPinia} from "pinia";
-import axios from 'axios';
 
-// Set up Axios to include credentials
-axios.defaults.withCredentials = true;
+const pina = createPinia()
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-axios.get('/sanctum/csrf-cookie')
-.then(() => {
-    const pina = createPinia()
-    const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-    createInertiaApp({
-        title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-        setup({ el, App, props, plugin }) {
-            return createApp({ render: () => h(App, props) })
-                .use(pina)
-                .use(plugin)
-                .use(ZiggyVue)
-                .mount(el);
-        },
-        progress: {
-            color: '#4B5563',
-        },
-    });
-})
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(pina)
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
