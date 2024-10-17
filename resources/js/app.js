255dcf5acce00b65fe5a9d9e04d5d3513e8d1442
Sarah B.M <sarah.melin19403@gmail.com>
2024-10-17 14:11:56 +0200

    fix delete @transfer

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import {createPinia} from "pinia";
-import axios from 'axios'
-
 
 const pinia = createPinia()
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -16,15 +14,11 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) });
-
-        // Set Axios globally
-        app.config.globalProperties.$axios = axios; // Make Axios globally available
-
-        app.use(pinia); // Use Pinia for state management
-        app.use(plugin); // Use Inertia plugin
-        app.use(ZiggyVue); // Use Ziggy for route management
-        app.mount(el); // Mount the app
+        return createApp({ render: () => h(App, props) })
+            .use(pinia)
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
     },
     progress: {
         color: '#4B5563',
--- resources/js/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-// Automatically attach CSRF token to Axios requests
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').content;
-
-export default axios;
